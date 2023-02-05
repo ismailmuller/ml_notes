b133beaf334cff684ecbd5ec649cbc3ae7f3c9b2
ismailmuller <43549523+ismailmuller@users.noreply.github.com>
2023-02-05 21:12:49 +0100

    Create autosklearn.py

---
+++ autosklearn.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
+import autosklearn.regression
+from sklearn.ensemble import RandomForestRegressor
+
+# Load data into a Pandas dataframe
+df = pd.read_csv("data.csv")
+
+# Split data into training and testing sets
+train_data, test_data, train_labels, test_labels = train_test_split(df.drop("target", axis=1), df["target"], test_size=0.33, random_state=42)
+
+# One-hot encode categorical variables
+enc = OneHotEncoder()
+train_data = enc.fit_transform(train_data)
+test_data = enc.transform(test_data)
+
+# Train the model
+automl = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=120, per_run_time_limit=30, tmp_folder='/tmp/autosklearn_regression_example_tmp', output_folder='/tmp/autosklearn_regression_example_out')
+automl.fit(train_data, train_labels)
+
+# Get the best model from auto-sklearn
+best_model = automl.get_models_with_weights()[0][0]
+
+# Plot the feature importances
+if isinstance(best_model, RandomForestRegressor):
+    importances = best_model.feature_importances_
+    indices = np.argsort(importances)
+    plt.barh(range(train_data.shape[1]), importances[indices])
+    plt.yticks(range(train_data.shape[1]), [enc.get_feature_names()[i] for i in indices])
+    plt.xlabel("Feature Importance")
+    plt.show()
